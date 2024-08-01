8c2065e4aa74368cceb8c359e783d0b9eea0f6cc
Dang <56210432+looks32@users.noreply.github.com>
2024-08-01 22:59:39 +0900

    layout 수정

--- app/list/[id]/layout.tsx
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-    title: "List",
-    description: '뉴욕 타임즈 베스트셀러 List',
-}
-
-export default function ListLayout({children}) {
-  return <>{children}</>;
-}
--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -1,9 +1,12 @@
-// "use client";
-
 import { useParams } from "next/navigation";
 import React, { Suspense } from "react";
 import BookList from "../../../components/BookList";
 import Loading from "../../../components/Loading";
+
+export const metadata = {
+    title: "List",
+    description: '뉴욕 타임즈 베스트셀러 List',
+}
 
 export default function BookListPage({params:id}) {
     return (
